b86d284e4df78ba7eaf3e7c92032caa7359f674d
Disadidi <hdisadidiconsulting@gmail.com>
2021-04-29 16:05:26 +0200

    Configuration swagger

--- src/main/java/com/hdconsulting/test/springboot/app/controllers/CuentaController.java
+++ src/main/java/com/hdconsulting/test/springboot/app/controllers/CuentaController.java
@@ -6,15 +6,14 @@ import com.hdconsulting.test.springboot.app.services.CuentaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
-@Controller
+
+@RestController
 @RequestMapping("/api/cuentas")
 public class CuentaController {
 
