304c68bd25f4f6c318e18b29e000feff661f3090
Disadidi <hdisadidiconsulting@gmail.com>
2021-04-30 23:20:02 +0200

    test d'integration guardar avec TestRestTemplate 2

--- src/test/java/com/hdconsulting/test/springboot/app/controllers/CuentaControllerTestRestTemplateTest.java
+++ src/test/java/com/hdconsulting/test/springboot/app/controllers/CuentaControllerTestRestTemplateTest.java
@@ -119,6 +119,20 @@ class CuentaControllerTestRestTemplateTest {
         assertEquals("2100.0", json.get(1).path("saldo").asText());
     }
 
+    @Test
+    @Order(4)
+    void testGuardar() {
+        Cuenta cuenta = new Cuenta(null, "Pepa", new BigDecimal("3800"));
+
+        ResponseEntity<Cuenta> respuesta = client.postForEntity(crearUri("/api/cuentas"), cuenta, Cuenta.class);
+        assertEquals(HttpStatus.CREATED, respuesta.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, respuesta.getHeaders().getContentType());
+        Cuenta cuentaCreada = respuesta.getBody();
+        assertNotNull(cuentaCreada);
+        assertEquals(3L, cuentaCreada.getId());
+        assertEquals("Pepa", cuentaCreada.getPersona());
+        assertEquals("3800", cuentaCreada.getSaldo().toPlainString());
+    }
 
     private String crearUri(String uri) {
         return "http://localhost:" + puerto + uri;
